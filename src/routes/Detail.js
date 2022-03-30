2ede9968cdf3a0793b7b00639f79989556fe2578
hseung578 <hseung578@gmail.com>
2022-03-30 17:13:18 +0900

    first

---
+++ src/routes/Detail.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+function Detail() {
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovies(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  });
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <div>
+            <a href={movies.url}>{movies.url}</a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+export default Detail;
